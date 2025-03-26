d8b651cfc0d91139f1d9689646368cf398994808
micky9889 <souligna9889@gmail.com>
2025-03-26 16:43:53 +0700

    'route_guard'

--- src/router/index.js
+++ src/router/index.js
@@ -12,5 +12,17 @@ const router = createRouter({
  
   ],
 })
+// Navigation guard to append ?nameUSER= if not present
+router.beforeEach((to, from, next) => {
+  const defaultName = 'guest';
+  if (!to.query.nameUSER) {
+    // Redirect to the same path with the query parameter
+    return next({
+      path: to.path,
+      query: { ...to.query, nameUSER: defaultName||"" }, // You can set a default value like 'guest' instead of ''
+    });
+  }
+  next(); // Proceed as normal if query parameter exists
+});
 
 export default router
